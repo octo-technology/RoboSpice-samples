a7d18ce25173181208c9c517521cc8c3d0252b92
Stéphane Nicolas <steff.nicolas@gmail.com>
2012-12-31 16:14:27 +0100

    All samples have been moved here.

---
+++ robospice-motivations/target/generated-sources/r/com/octo/android/robospice/motivations/BuildConfig.java
@@ -0,0 +1,5 @@
+package com.octo.android.robospice.motivations;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
---
+++ robospice-sample-google-http-client/target/generated-sources/r/com/octo/android/robospice/sample/googlehttpclient/BuildConfig.java
@@ -0,0 +1,5 @@
+package com.octo.android.robospice.sample.googlehttpclient;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
