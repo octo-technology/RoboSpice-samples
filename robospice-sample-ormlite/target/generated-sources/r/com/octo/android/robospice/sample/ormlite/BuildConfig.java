c09d4c4c7857c65facc1e61e6136589529454e64
Riccardo Ciovati <rciovati@gmail.com>
2013-01-02 13:50:58 +0100

    cleanup

--- robospice-sample-ormlite/target/generated-sources/r/com/octo/android/robospice/sample/ormlite/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package com.octo.android.robospice.sample.ormlite;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
--- robospice-sample-ui-spicelist/target/generated-sources/r/com/octo/android/robospice/sample/ui/spicelist/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package com.octo.android.robospice.sample.ui.spicelist;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
