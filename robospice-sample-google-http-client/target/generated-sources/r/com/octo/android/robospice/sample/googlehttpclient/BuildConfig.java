039202b1d17965c40e7b9e0621516c7c4bf2d916
Stéphane Nicolas <steff.nicolas@gmail.com>
2013-01-02 16:20:07 +0100

    Added lint check to samples and removed target directories

--- robospice-sample-google-http-client/target/generated-sources/r/com/octo/android/robospice/sample/googlehttpclient/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package com.octo.android.robospice.sample.googlehttpclient;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
--- robospice-sample-spring-android/target/generated-sources/r/com/octo/android/robospice/sample/springandroid/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package com.octo.android.robospice.sample.springandroid;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
