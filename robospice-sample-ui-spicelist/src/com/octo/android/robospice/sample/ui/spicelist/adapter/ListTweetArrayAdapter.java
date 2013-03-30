c08423b9bcde28ddee17bfa8ed5852360bd6dfd2
Stéphane Nicolas <steff.nicolas@gmail.com>
2013-03-30 08:56:17 +0100

    Support for issue #76 of RS

--- robospice-sample-ui-spicelist/src/com/octo/android/robospice/sample/ui/spicelist/adapter/ListTweetArrayAdapter.java
+++ robospice-sample-ui-spicelist/src/com/octo/android/robospice/sample/ui/spicelist/adapter/ListTweetArrayAdapter.java
@@ -3,6 +3,7 @@ package com.octo.android.robospice.sample.ui.spicelist.adapter;
 import java.io.File;
 
 import android.content.Context;
+import android.view.ViewGroup;
 
 import com.octo.android.robospice.request.simple.BitmapRequest;
 import com.octo.android.robospice.sample.ui.spicelist.model.ListTweets;
@@ -24,18 +25,21 @@ public class ListTweetArrayAdapter extends SpiceArrayAdapter<Tweet> {
     // CONSTRUCTOR
     // --------------------------------------------------------------------------------------------
 
-    public ListTweetArrayAdapter(Context context, BitmapSpiceManager spiceManagerBitmap, ListTweets tweets) {
+    public ListTweetArrayAdapter(Context context, BitmapSpiceManager spiceManagerBitmap,
+        ListTweets tweets) {
         super(context, spiceManagerBitmap, tweets.getResults());
     }
 
     @Override
     public BitmapRequest createRequest(Tweet tweet, int requestImageWidth, int requestImageHeight) {
-        File tempFile = new File(getContext().getCacheDir(), "THUMB_IMAGE_TEMP_" + tweet.getFrom_user());
-        return new BitmapRequest(tweet.getProfile_image_url(), requestImageWidth, requestImageHeight, tempFile);
+        File tempFile = new File(getContext().getCacheDir(), "THUMB_IMAGE_TEMP_"
+            + tweet.getFrom_user());
+        return new BitmapRequest(tweet.getProfile_image_url(), requestImageWidth,
+            requestImageHeight, tempFile);
     }
 
     @Override
-    public SpiceListItemView<Tweet> createView(Context context) {
+    public SpiceListItemView<Tweet> createView(Context context, ViewGroup parent) {
         return new TweetListItemView(getContext());
     }
 }
