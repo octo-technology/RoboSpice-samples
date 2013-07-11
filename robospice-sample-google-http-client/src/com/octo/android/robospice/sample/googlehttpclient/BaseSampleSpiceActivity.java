006e2e8ff93011bea105bd76e57a3c0a2de44ce3
Stéphane Nicolas <steff.nicolas@gmail.com>
2013-07-12 00:39:24 +0200

    Added pre-set services for Google http client module.

--- robospice-sample-google-http-client/src/com/octo/android/robospice/sample/googlehttpclient/BaseSampleSpiceActivity.java
+++ robospice-sample-google-http-client/src/com/octo/android/robospice/sample/googlehttpclient/BaseSampleSpiceActivity.java
@@ -2,24 +2,22 @@ package com.octo.android.robospice.sample.googlehttpclient;
 
 import android.app.Activity;
 
+import com.octo.android.robospice.JacksonGoogleHttpClientSpiceService;
 import com.octo.android.robospice.SpiceManager;
 
 /**
- * This class is the base class of all activities of the sample project. This class offers all subclasses an easy access
- * to a {@link SpiceManager} that is linked to the {@link Activity} lifecycle.
- * 
- * Typically, in a new project, you will have to create a base class like this one and copy the content of the
- * {@link BaseSampleSpiceActivity} into your own class.
- * 
+ * This class is the base class of all activities of the sample project. This class offers all
+ * subclasses an easy access to a {@link SpiceManager} that is linked to the {@link Activity}
+ * lifecycle. Typically, in a new project, you will have to create a base class like this one and
+ * copy the content of the {@link BaseSampleSpiceActivity} into your own class.
  * @author sni
- * 
  */
 public abstract class BaseSampleSpiceActivity extends Activity {
-    private SpiceManager spiceManager = new SpiceManager( SampleSpiceService.class );
+    private SpiceManager spiceManager = new SpiceManager(JacksonGoogleHttpClientSpiceService.class);
 
     @Override
     protected void onStart() {
-        spiceManager.start( this );
+        spiceManager.start(this);
         super.onStart();
     }
 
